f9e0b88a4aac4c8fa218d926b13c144f6d8459d3
EkaterinaBriskova <ekaterinabriskova1@gmail.com>
2025-04-02 20:10:08 +0300

    Практика №2

---
+++ multiactivity/src/main/java/com/mirea/briskovaekaterinaborisovna/multiactivity/MainActivity.java
@@ -0,0 +1,25 @@
+package com.mirea.briskovaekaterinaborisovna.multiactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickNewActivity(View view) {
+        EditText editText = findViewById(R.id.editText);
+        String text = editText.getText().toString();
+
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("key", text);
+        startActivity(intent);
+    }
+}
